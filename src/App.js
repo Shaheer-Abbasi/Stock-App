0300ee60ec5e290aa43eb8c3decaddc3e74cc423
Shaheer <abbasi.shaheer14@gmail.com>
2022-07-18 18:26:46 -0500

    init commit
    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='title' id='t1'>Hello World</h1>
+      <h1 className='title' id='t2'>Title 2</h1>
+    </div>
+  );
+}
+
+export default App;
