c68a9670ea25820605f47855eb7d104249b91a1e
Shaheer <abbasi.shaheer14@gmail.com>
2023-08-27 14:10:21 -0500

    test commit

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className='title' id='t1'>Hello World</h1>
-      <h1 className='title' id='t2'>Title 2</h1>
-      
+      <h1 className='title' id='t1'>Sticky Stocks</h1>
+      <h1 className='title' id='t2'>Top 50 Stocks currently</h1>
+     
     </div>
   );
 }
