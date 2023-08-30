ae68220e537740500a397af53e6d04c0419778aa
Shaheer <abbasi.shaheer14@gmail.com>
2023-08-30 18:49:02 -0500

    add basic css

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header()
+{
+    return (
+        <header className="header">
+            <h2 className="header--text">Stock Sim</h2>
+            <img className="dollar--image" alt="" src="icons8-dollar-sign-48.png"/>
+        </header>
+    )
+}
--- src/components/Main.js
+++ src/components/Main.js
@@ -4,7 +4,19 @@ export default function Main()
 {
     return (
         <div>
-            <h1>This Main is working!</h1>
+            <h4 className="main--text">What company's stock do you want to buy?</h4>
+            <input
+                className="main--search"
+                type="search" 
+                placeholder="Search"
+            />
+            <h4 className="how-much">What percent of the stock do you want to buy?</h4> 
+            <input
+                className="main--select"
+                type="textbox" 
+                placeholder="Enter precent"
+            />         
+            <h4 className="main--revenue">Your revenue:</h4>
         </div>
     )
 }
